bd17835882c13f27275c5fe7a78bef937ee5287e
Mrinal Verma <mrinalverma1993@gmail.com>
2025-07-21 15:24:48 +0200

    Created Mood Model and Mood Checkin Model Backend

--- bean_backend/coffee/models.py
+++ bean_backend/coffee/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 class Mood(models.Model):
     name = models.CharField(max_length=50)
-    coffee_recommendation = models.TextField
+    coffee_recommendation = models.TextField(blank=True)
     emoji = models.CharField(max_length=10, default='☕')
 
     def __str__(self):
-        return self.name
+        return self.emoji
+
+class MoodCheckin(models.Model):
+    mood = models.ForeignKey(Mood, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    note = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.mood.name
--- bean_backend/coffee/serializers.py
+++ bean_backend/coffee/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from .models import Mood
+from .models import Mood, MoodCheckin
 
 class MoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mood
         fields = '__all__'
+
+class MoodCheckinSerializer(serializers.ModelSerializer):
+    mood_name = serializers.CharField(source='mood.name', read_only=True)
+    mood_emoji = serializers.CharField(source='mood.emoji', read_only=True)
+    coffee_recommendation = serializers.CharField(source='mood.coffee_recommendation', read_only=True)
+
+    class Meta:
+        model = MoodCheckin
+        fields = ['id', 'mood', 'mood_name', 'mood_emoji', 'coffee_recommendation', 'created_at', 'note']
